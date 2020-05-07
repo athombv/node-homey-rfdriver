4c9d2915d16e47909a76e5310b64e281168f79d2
Emile Nijssen <emile@athom.nl>
2020-05-07 21:57:04 +0200

    wip

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports.RFDevice = require('./lib/RFDevice');
+module.exports.RFDriver = require('./lib/RFDriver');
+module.exports.RFSignal = require('./lib/RFSignal');
+module.exports.RFUtil = require('./lib/RFUtil');
+module.exports.RFError = require('./lib/RFError');
